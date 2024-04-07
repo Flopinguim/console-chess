35d9f229010ea11fe57ea139f9487fc1cecc29a4
Pingui <08joaovitor01@gmail.com>
2024-04-07 20:24:21 -0300

    feat: Criação da classe Posicao

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Tabuleiro;
+
+namespace console_chess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao posicao;
+            posicao = new Posicao(3,4)
+        }
+    }
+}
