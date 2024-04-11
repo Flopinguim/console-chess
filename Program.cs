19a92d507ff97a5fb2f613d32677a12c9e7460ba
Pingui <08joaovitor01@gmail.com>
2024-04-11 01:50:09 -0300

    Tratando erros com exececao personalizada

--- Program.cs
+++ Program.cs
@@ -7,10 +7,17 @@ namespace console_chess
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Preta), new Posicao(0, 0));
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
 
-            Tela.imprimirTabuleiro(tabuleiro);
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
         }
     }
---
+++ tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+﻿namespace console_chess.tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public  TabuleiroException(string mensagem) : base(mensagem)
+        {
+
+        }
+    }
+}
